02581aa108f6075d5cc2d49585b93278d614814c
Sanford, Eliot <vc.eliot.sanford@lowes.com>
2021-06-30 07:34:13 -0500

    fix: send empty array

--- index.js
+++ index.js
@@ -3,34 +3,9 @@ const morgan = require('morgan');
 const port = 3000
 
 const app = express();
-// app.use(morgan('common'));
+app.use(morgan('common'));
 
-const users = []
-
-// const users = [{
-// 	// id: 1,
-// 	name: 'Micheal Bluth'
-// },
-// {
-// 	// id: 2,
-// 	name: 'Gob Bluth'
-// },
-// {
-// 	// id: 3,
-// 	name: 'Lucille Bluth'
-// },
-// {
-// 	// id: 4,
-// 	name: 'Lindsay Bluth Fünke'
-// },
-// {
-// 	// id: 5,
-// 	name: 'Tobia Fünke'
-// },
-// {
-// 	// id: 6,
-// 	name: 'George Bluth'
-// }]
+const users = [{user: 'Michael'}, {user:'Gob'},{ user: 'Lucille'}, {user: 'George'}]
 
 app.get('/', (req, res) => {
 	console.log("-----")
@@ -39,14 +14,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-	// console.log("-----")
-	// console.log("successfully fetched a user list!");
-	users.push('Michael')
-	users.push('Gob')
-	users.push('Lucille')
-	users.push('George')
+	console.log("-----")
+	console.log("successfully fetched a user list!");
 	console.log(users)
-	res.json(users);
+	res.send([]);
 })
 
 app.listen(port, err => {
