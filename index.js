4830bcda885fa54a0ce8a7a3f7a23a0d880e07af
Eliot Sanford <eliot587@gmail.com>
2021-07-18 14:11:31 -0500

    build: upgrade packages and install yarn

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/users', (req, res) => {
 	res.send([]);
 })
 
+app.post('/users', (req, res) => {
+	console.log('loading...');
+	console.log('body:', req.body);
+	req.send(req.body);
+})
+
 app.listen(port, err => {
 	if (err) console.log(err);
 	console.log(`Example app listening at http://localhost:${port}`)
